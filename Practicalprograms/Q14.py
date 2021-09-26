cce3d1a24a0c036a45d2b74376c3a1acbcfee2a8
Siddheshwarhotge <91409035+Siddheshwarhotge@users.noreply.github.com>
2021-09-26 13:56:57 +0530

    Create Q14.py

---
+++ Practicalprograms/Q14.py
@@ -0,0 +1,12 @@
+#Write Program to Check Whether a Number is Even or Odd - 88,37,1658
+
+x=88371658
+
+if (x % 2) == 0 :
+    print("Number is Even ",x)
+else :
+    print("Number is Odd ",x)
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
