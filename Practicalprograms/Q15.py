e65c32819c8ca8267dcef3a791d2c7de96c1582c
Siddheshwarhotge <91409035+Siddheshwarhotge@users.noreply.github.com>
2021-09-26 13:57:25 +0530

    Create Q15.py

---
+++ Practicalprograms/Q15.py
@@ -0,0 +1,13 @@
+#Check whether an alphabet is vowel or consonant using if..else statement - a,b,e,o
+
+x='a'
+
+if x in ('a','e','i','o','u') :
+    print("The given Alphabet is vowel",x)
+else :
+    print("The given Alphabet is consonant",x)
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Sta
